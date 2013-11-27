9832c424c1d9639304509b81e5745f5c4f2549a4
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2013-11-27 22:33:00 +0100

    correct type

--- generator.py
+++ generator.py
@@ -8,9 +8,9 @@ template = env.get_template('redis.conf')
 
 port = 0000
 db_number = 10
-db_id = "398498734987"
+db_id = 398498734987
 password = 'foobar'
-filename = "configurations/%s.conf" % db_id
+filename = "configurations/%i.conf" % db_id
 content = template.render(port = port, db_number = db_number, db_id = db_id, password = password)
 
 file = open(filename, "w")
