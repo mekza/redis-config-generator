90e601654a2a21a544f5b89eafff872e427f5831
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2013-11-28 10:51:03 +0100

    refactoring

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Redis Configuration Generator
 
 Quick and dirty redis configuration generator.
+
+
+Usage:
+    $ ./generator.py 22 8000 5 foobar
+    $ Your file is generated: configurations/22.conf
--- generator.py
+++ generator.py
@@ -2,18 +2,39 @@
 # -*- coding:utf-8 -*-
 
 from jinja2 import Environment, PackageLoader
+import sys
+
 env = Environment(loader=PackageLoader('generator', 'templates'))
 
 template = env.get_template('redis.conf')
 
-port = 0000
-db_number = 10
-db_id = 398498734987
-password = 'foobar'
-filename = "configurations/%i.conf" % db_id
-content = template.render(port = port, db_number = db_number, db_id = db_id, password = password)
+def main():
+    if len(sys.argv) != 5:
+        print ''
+        print 'Usage: %s <DB_ID> <PORT> <DB_NUMBER>  <PASSWORD>' % (sys.argv[0],)
+        print ''
+        print 'DB_ID: ID or name'
+        print 'PORT: TCP Port'
+        print 'DB_NUMBER: Number of DB'
+        print 'PASSWORD: Password'
+        print ''
+        sys.exit(1)
 
-file = open(filename, "w")
+    db_id = int(sys.argv[1])
+    port = sys.argv[2]
+    db_number = sys.argv[3]
+    password = sys.argv[4]
+    filename = "configurations/%i.conf" % db_id
 
-file.write(content)
-file.close()
+    content = template.render(port = port, db_number = db_number, db_id = db_id, password = password)
+
+    try:
+        file = open(filename, "w")
+        file.write(content)
+        file.close()
+        print "Your file is generated: %s" % filename
+    except IOError:
+        print IOError
+
+if __name__ == '__main__':
+    main()
